f727629e3648096e7999b321f721a50315864344
raymarion <zr3421500@126.com>
2015-01-17 16:20:28 +0800

    1st

---
+++ src/main/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author ray
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str1 = "abs";
+		String str2 = str1;//"abs";
+		str1 = str1+"123";
+		System.out.println(str1);
+		System.out.println(str2);
+		
+
+	}
+
+}
---
+++ src/packageA/ClassB.java
@@ -0,0 +1,17 @@
+package packageA;
+
+public class ClassB extends ClassA {
+	
+	int noTag;
+	
+	int pub;
+	
+	
+	
+		
+		
+		
+	
+	
+
+}
